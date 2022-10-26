38a1b508cff63312f58503814f1813f83975225b
Luke <luke.lukejoe@gmail.com>
2022-10-25 21:24:05 -0700

    Updated portfolio item containers

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,20 +5,21 @@ const projects = () => {
   return (
     <>
       <div className="flex justify-center items-center dynamic-card">
-        <div className="bg-slate-800 text-slate-200 px-16 sm:max-w-xl">
-          <h1 className="text-3xl uppercase font-black">Projects</h1>
+        <div className="bg-slate-800 text-slate-200 sm:px-0 px-9 sm:max-w-xl">
+          <h1 className="text-3xl uppercase font-black py-6">Projects</h1>
 
           <div className="portfolio-container">
-            <article className="portfolio-item">
-              <div className="portfolio-image">
-                <img src={IMG1} alt="" />
+            <div className="portfolio-item">
+              <img src={IMG1} className="portfolio-image" alt="" />
+              <div className="flex items-center justify-center">
+                <h3>Mondo Dodgeball</h3>
               </div>
-              <h3>Title</h3>
-              <div className="portfolio-links">
-                <a className="btn">Github</a>
-                <a>Demo</a>
+              <div className="flex items-center justify-center">
+                <p className="text-center">
+                  This is a description of what this app does
+                </p>
               </div>
-            </article>
+            </div>
           </div>
         </div>
       </div>
